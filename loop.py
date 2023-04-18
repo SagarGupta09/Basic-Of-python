5f8aa168778ee5a961c56f601e40920078a082f3
Sagar <125907266+SagarGupta09@users.noreply.github.com>
2023-04-18 14:33:33 +0530

    Update loop.py

--- loop.py
+++ loop.py
@@ -1,9 +1,9 @@
-n1 = input("Enter your numbers :")
+n1 = input("Enter your numbers: ")
 l = list(n1)
 l1 = len(l)
-n2 = {0: "zero", 1:"one", 2 : "two", 3: "three", 4: "four"}
-i = 0;
-while i<l1:
-    print(n2[int(l[i])], end=" ")
-    i = i+1
+n2 = {0: "zero", 1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven", 8: "eight", 9: "nine"}
+i = 0
+while i < l1:
+    print(n2[int(l[i])])
+    i = i + 1
 
