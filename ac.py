8a8abbffaca2447118dcb0e5c3a6d6f542dd2b97
Sagar Gupta <Sagargupta0977@gamil.com>
2023-04-18 14:16:42 +0530

    basic

---
+++ ac.py
@@ -0,0 +1,34 @@
+# # num=int(input("Enter any decimal number:"))
+# # rem=0
+# # sum=0
+# # while(num>0):
+# #     rem=num%10
+# #     num=num//10
+# #     sum=sum+rem
+# # print("sum=",sum)
+
+# # x=4
+# # y=x+1
+# # x=2
+# # print(x)
+# # print(y)
+
+# # a=2
+# # b=3
+# # c=a
+# # b=c+1
+# # print(a)
+# # print(b)
+# # print(c)
+
+
+# # x=6
+# # print(x)
+# # print("6")
+
+# a=int(input("Enter any integer number:"))
+# b=float(input("Enter Float value"))
+# result=a*b
+# print("product=",result)
+
+print("A\nB\nC\nD\nE\nF")
